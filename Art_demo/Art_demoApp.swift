924b8f660af6d109af15e70b3a5eb8247434b832
杨洁 <xydediannao@xydediannaodeMacBook-Pro.local>
2022-11-26 23:07:13 +0800

    v1.0

---
+++ Art_demo/Art_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_demoApp.swift
+//  Art_demo
+//
+//  Created by xy的电脑 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Art_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
